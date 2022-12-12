ab61b5819e44fd4c9870e17d70e96454c043c48d
Amit Kumar <amitcute3@gmail.com>
2022-12-12 20:06:30 +0530

    Create useRestorables.ts

---
+++ useRestorables.ts
@@ -0,0 +1,66 @@
+import * as React from "react";
+
+type Options = {
+  id: string,
+  initialValue: any,
+  shouldRestoreOldValue?: boolean,
+};
+
+const useRestorableValue = ({ id, initialValue, shouldRestoreOldValue = true }:Options) => {
+  const ref = React.useRef(initialValue);
+  const [value, setValue] = React.useState(initialValue);
+
+  // load initial value from window storage or by given value
+  const loadInitialValue = () => {
+    const windowObj = window;
+
+    if (!windowObj.restorableValueStorage) {
+      windowObj.restorableValueStorage = {};
+    }
+
+    if (shouldRestoreOldValue && windowObj.restorableValueStorage[id]) {
+      ref.current = windowObj.restorableValueStorage[id];
+      setValue(ref.current);
+    } else {
+      windowObj.restorableValueStorage[id] = initialValue;
+      ref.current = initialValue;
+    }
+  };
+
+  React.useEffect(() => {
+    loadInitialValue();
+  }, [id]);
+
+  // Reset value to initial value
+  const resetValue = () => {
+    const windowObj = window;
+
+    windowObj.restorableValueStorage[id] = initialValue;
+    ref.current = initialValue;
+
+    setValue(ref.current);
+  };
+
+  // Restore value to previously applied value
+  const restoreValue = () => {
+    setValue(ref.current);
+  };
+
+  // Apply current value to state and storage
+  const applyValue = (value) => {
+    const windowObj = window;
+
+    ref.current = value;
+    windowObj.restorableValueStorage[id] = value;
+
+    setValue(value);
+  };
+
+  return {
+    value,
+    resetValue,
+    restoreValue,
+    applyValue,
+    setValue
+  };
+};
